9b486632c9ecd1d2c24d1ed320c367dbb805f0f4
Voi <wojtek.wacek.dola@gmail.com>
2023-05-06 14:00:27 +1000

    Created the basic structure in server.js

--- server.js
+++ server.js
@@ -1,3 +1,36 @@
+const express = require("express");
+const mysql = require("mysql2");
+
+// Import and require user-input
 const { run } = require("./lib/user-input.js");
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // TODO: Add MySQL password here
+    password: "Shire2023**",
+    database: "staff_db",
+  },
+  console.log(`Connected to the staff_db database.`)
+);
+
 run();
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
